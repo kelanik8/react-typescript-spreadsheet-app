67c8a1caa3108e021407d5a79ec96881bb696e6f
Kelani Tolulope <kelanik8@gmail.com>
2022-11-23 02:17:57 +0100

    feat(tokenization): interpreting arithmetic lexical analysis

--- src/formula.ts
+++ src/formula.ts
@@ -1,3 +1,7 @@
 export function calculateValueOf<T>(content: T): T {
   return content;
 }
+
+export function tokenize(content: string): string[] {
+  return content.match(/\d+(\.\d+)?|[+\-*/]|[^\s]+/g) ?? [];
+}
--- test/formulas.test.ts
+++ test/formulas.test.ts
@@ -1,11 +1,35 @@
-import { calculateValueOf } from "../src/formula";
+import { calculateValueOf, tokenize } from "../src/formula";
 
-describe("Formulas", () => {
+describe("formulas", () => {
   it("does not calculate anything for empty cells", () => {
     expect(calculateValueOf(undefined)).toEqual(undefined);
   });
 
   it("does not calculate anything for fixed values", () => {
     expect(calculateValueOf("some content")).toEqual("some content");
+  });
+
+  xit("calculates simple arithmetic", () => {
+    expect(calculateValueOf("=1+2")).toEqual("3");
+  });
+
+  describe("tokenize", () => {
+    it("has nothing to do for an empty string", () => {
+      expect(tokenize("")).toEqual([]);
+    });
+
+    it("tokenizes numbers", () => {
+      expect(tokenize("1")).toEqual(["1"]);
+      expect(tokenize("12.34")).toEqual(["12.34"]);
+      expect(tokenize("-1.23")).toEqual(["-", "1.23"]);
+    });
+
+    it("tokenizes operators", () => {
+      expect(tokenize("+-*/")).toEqual(["+", "-", "*", "/"]);
+    });
+
+    it("tokenizes thing that are unexpected", () => {
+      expect(tokenize("unexpected #!@")).toEqual(["unexpected", "#!@"]);
+    });
   });
 });
